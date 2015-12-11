fa8fab4cdb17da2efa01ea3c27795a150a637493
Explomat <cabehok@inbox.ru>
2015-12-11 18:32:57 +0300

    Cahnge server

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 <%
 var KNOLEDGE_TYPE = 1; //знание
 
-var startSourcePercent = 50;
-
-function getDestPersent(_startPercent, _startSourcePercent){
-	_startPercent = Int(_startPercent);
-	_startSourcePercent = _startSourcePercent == undefined ? startSourcePercent : Int(_startSourcePercent);
-	if (_startPercent < 50) return 0;
-	var diff = _startPercent - _startSourcePercent;
-	return diff * 2;
-}
-
 function stringifyWT(obj) {
 	var type = DataType(obj);
 	var curObj = obj;
@@ -52,16 +42,12 @@ function getAssessment(queryObjects){
 	var competences = OpenDoc(UrlFromDocID(Int(pa.id))).TopElem.competences;
 	for (c in competences){
 		compName = OpenDoc(UrlFromDocID(Int(c.competence_id))).TopElem.name;
-		compObj = { id: c.competence_id + '', cols: [compName + ''], children: [] };
-		compValue = 0;
+		compObj = { id: c.competence_id + '', cols: [compName + '', StrReal(c.mark_value, 2)], children: [] };
 		for (i in c.indicators) {
 			indDoc = OpenDoc(UrlFromDocID(Int(i.indicator_id)));
 			markValue = i.mark_value == null ? 0 : i.mark_value;
-			compValue = compValue + (indDoc.TopElem.type == KNOLEDGE_TYPE ? getDestPersent(markValue) : markValue);
 			compObj.children.push({ id: i.indicator_id + '', cols: [ StrReplace(indDoc.TopElem.name + '', '"', ''), Int(markValue) ] });
 		}
-		compValue = compValue / ArrayCount(c.indicators);
-		compObj.cols.push(Int(compValue) + '');
 		outCompentences.push(compObj);
 	}
 	return stringifyWT({ collaborators: outCompentences});
